f408a91992981eca6467f083255cc0053d415287
Jaehyun <jayangie@naver.com>
2022-08-17 20:55:30 +0900

    looping finished

--- src/demo.py
+++ src/demo.py
@@ -29,96 +29,44 @@ def demo(args):
         img_list.extend(glob(os.path.join("images", ext)))
     img_list.sort()
 
+    mask_list = []
+    for ext in ['*.jpg', '*.png']:
+        mask_list.extend(glob(os.path.join("mask", ext)))
+
     # Model and version
     net = importlib.import_module('model.'+args.model)
     model = net.InpaintGenerator(args)
     model.load_state_dict(torch.load("../experiments/G0000000.pt", map_location='cpu'))
     model.eval()
 
-    for fn in img_list:
+    for (fn, mk) in zip(img_list, mask_list):
         filename = os.path.basename(fn).split('.')[0]
         orig_img = cv2.imread(fn, cv2.IMREAD_COLOR)
         img_tensor = (ToTensor()(orig_img) * 2.0 - 1.0).unsqueeze(0)
         h, w, c = orig_img.shape
         mask = np.zeros([h, w, 1], np.uint8)
-        image_copy = orig_img.copy()
-        sketch = Sketcher(
-            'input', [image_copy, mask], lambda: ((255, 255, 255), (255, 255, 255)), args.thick, "freeform")
 
         while True:
-            ch = cv2.waitKey()
-            if ch == 27:
-                print("quit!")
-                break
+            print('[**] inpainting ... ')
+            print(f"{mask.shape}")
 
-            # inpaint by deep model
-            elif ch == ord(' '):
-                print('[**] inpainting ... ')
-                print(f"{mask.shape}")
+            with torch.no_grad():
+                print(f"mask info: {type(mask), mask.shape}")
+                mname = os.path.basename(mk).split('.')[0]
+                mask = Image.open(mname)
+                mask_tensor = (ToTensor()(mask)).unsqueeze(0)
+                masked_tensor = (img_tensor * (1 - mask_tensor).float()) + mask_tensor
 
-                # m = Image.fromarray(mask)
-                # m.save('mask.png')
-                with torch.no_grad():
-                    print(f"mask info: {type(mask), mask.shape}")
-                    mask = mask.squeeze()
-                    tt = Image.fromarray(mask)
-                    tt.save('mask.png')
-                    exit()
-                    mask_tensor = (ToTensor()(mask)).unsqueeze(0)
-                    masked_tensor = (img_tensor * (1 - mask_tensor).float()) + mask_tensor
+                pred_tensor = model(masked_tensor, mask_tensor)
+                comp_tensor = (pred_tensor * mask_tensor + img_tensor * (1 - mask_tensor))
 
-                    pred_tensor = model(masked_tensor, mask_tensor)
-                    comp_tensor = (pred_tensor * mask_tensor + img_tensor * (1 - mask_tensor))
+                pred_np = postprocess(pred_tensor[0])
+                masked_np = postprocess(masked_tensor[0])
+                comp_np = postprocess(comp_tensor[0])
 
-                    pred_np = postprocess(pred_tensor[0])
-                    masked_np = postprocess(masked_tensor[0])
-                    comp_np = postprocess(comp_tensor[0])
-
-                    cv2.imshow('pred_images', comp_np)
-                    im = Image.fromarray(comp_np)
-                    im.save(f"{filename}_inpainted.png")
-                    print('inpainting finish!')
-            
-            # reset mask
-            elif ch == ord('r'):
-                img_tensor = (ToTensor()(orig_img) * 2.0 - 1.0).unsqueeze(0)
-                image_copy[:] = orig_img.copy()
-                mask[:] = 0
-                sketch.show()
-                print("[**] reset!")
-
-            # next case
-            elif ch == ord('n'):
-                print('[**] move to next image')
-                cv2.destroyAllWindows()
-                break
-
-            elif ch == ord('k'): 
-                print('[**] apply existing processing to images, and keep editing!')
-                img_tensor = comp_tensor
-                image_copy[:] = comp_np.copy()
-                mask[:] = 0
-                sketch.show()
-                print("reset!")
-            
-            elif ch == ord('+'): 
-                sketch.large_thick()
-
-            elif ch == ord('-'): 
-                sketch.small_thick()
-            
-            # save results
-            if ch == ord('s'):
-                cv2.imwrite(os.path.join(args.outputs, f'{filename}_masked.png'), masked_np)
-                cv2.imwrite(os.path.join(args.outputs, f'{filename}_pred.png'), pred_np)
-                cv2.imwrite(os.path.join(args.outputs, f'{filename}_comp.png'), comp_np)
-                cv2.imwrite(os.path.join(args.outputs, f'{filename}_mask.png'), mask)
-
+                cv2.imwrite(filename+"_inpainted.png", comp_np)
+                print('inpainting finish!')
                 print('[**] save successfully!')
-        cv2.destroyAllWindows()
-
-        if ch == 27:
-            break
 
 
 if __name__ == '__main__':
